bd7b1cf3d8d1c7690d8e6c36e218df1482369a4a
rudrajoshi-js <rudra.joshi@jerseystem.org>
2026-02-12 16:25:46 -0500

    Simplify service worker fetch event handling
    Removed caching logic and simplified fetch handling.

--- sw.js
+++ sw.js
@@ -1,30 +1,13 @@
 const CACHE_NAME = "inventory-pwa-v1";
 
-const FILES_TO_CACHE = [
-  "/",
-  "/index.html",
-  "/app.js",
-  "/manifest.json"
-];
-
-// Install
 self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
-  );
   self.skipWaiting();
 });
 
-// Activate
 self.addEventListener("activate", (event) => {
   event.waitUntil(self.clients.claim());
 });
 
-// Fetch
 self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
+  event.respondWith(fetch(event.request));
 });
